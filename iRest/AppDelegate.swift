5ef65adff8b028d336dc4b6d9ab957b90d8df194
Maayan <maayan.albert1@gmail.com>
2019-01-05 15:34:39 -0500

    Initial Commit

---
+++ iRest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iRest
+//
+//  Created by Maayan Albert on 1/5/19.
+//  Copyright © 2019 Maayan Albert. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iRest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iRest
+//
+//  Created by Maayan Albert on 1/5/19.
+//  Copyright © 2019 Maayan Albert. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
